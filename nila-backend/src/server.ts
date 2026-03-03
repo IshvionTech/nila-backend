2b6c3515b1c025750c966c27c58e4e0df4859c3f
siva <sivasakthihbk@gmail.com>
2026-03-03 12:51:43 +0530

    correcting cors

--- nila-backend/src/server.ts
+++ nila-backend/src/server.ts
@@ -12,7 +12,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: "https://your-frontend-name.netlify.app",
+  credentials: true
+}));
+
 app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/dashboard", dashboardRoutes);
