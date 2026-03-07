df0db41543fedc4c9a31e3721df83cb06675fa61
siva <sivasakthihbk@gmail.com>
2026-03-07 15:07:27 +0530

    checked

--- nila-backend/src/server.ts
+++ nila-backend/src/server.ts
@@ -13,7 +13,7 @@ const app = express();
 
 
 const corsOptions = {
-  origin: "https:/lucent-axolotl-e55b80.netlify.app",
+  origin: "https://clinquant-kleicha-d692c7.netlify.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
