c442562ecc4ea81d98b9392426c9e9e48131f071
siva <sivasakthihbk@gmail.com>
2026-03-07 12:59:42 +0530

    upadted

--- nila-backend/src/server.ts
+++ nila-backend/src/server.ts
@@ -13,7 +13,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: "https://neon-lollipop-0c6945.netlify.app",
+  origin: "https://deft-lamington-5b55bf.netlify.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
