448ccd4255733ffd7298cedccbcb20f04f0a8dfb
siva <sivasakthihbk@gmail.com>
2026-03-07 14:44:52 +0530

    cros

--- nila-backend/src/server.ts
+++ nila-backend/src/server.ts
@@ -13,7 +13,7 @@ const app = express();
 
 
 const corsOptions = {
-  origin: "https://symphonious-cascaron-e21ed5.netlify.app",
+  origin: "https://elaborate-babka-ab6730.netlify.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
