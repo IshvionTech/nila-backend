2ec7457e4dc3f7d64e4f3b16c8cee24519b82028
siva <sivasakthihbk@gmail.com>
2026-03-09 12:13:18 +0530

    fix cors

--- nila-backend/src/server.ts
+++ nila-backend/src/server.ts
@@ -13,7 +13,8 @@ const app = express();
 
 
 const corsOptions = {
-  origin:"https://iridescent-centaur-2be2f7.netlify.app",
+  origin:["https://iridescent-centaur-2be2f7.netlify.app",
+  "https://neon-lollipop-0c6945.netlify.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
