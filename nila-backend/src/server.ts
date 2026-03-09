371df946269159c084e86d8e885533e4245e6712
siva <sivasakthihbk@gmail.com>
2026-03-09 12:08:47 +0530

    changed

--- nila-backend/src/server.ts
+++ nila-backend/src/server.ts
@@ -14,7 +14,6 @@ const app = express();
 
 const corsOptions = {
   origin:"https://iridescent-centaur-2be2f7.netlify.app",
-  //origin: "https://clinquant-kleicha-d692c7.netlify.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
@@ -24,14 +23,6 @@ app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));   // fix preflight request
 
 
-// app.use(cors({
-//   origin: "https://deft-lamington-5b55bf.netlify.app",
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-//   credentials: true
-// }));
-
-// app.options("*", cors());
 
 
 //app.use(cors());
