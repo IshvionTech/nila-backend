315e6fe11bf9ee99cf426bf1eae8f958d310fb68
siva <sivasakthihbk@gmail.com>
2026-03-11 11:20:27 +0530

    fix experts

--- nila-backend/src/controllers/expert.controller.ts
+++ nila-backend/src/controllers/expert.controller.ts
@@ -4,7 +4,7 @@ import  pool  from "../db";
 export const getExperts = async (req: Request, res: Response) => {
      //console.log("getExperts function called"); // Add this line
   try {
-    const result = await pool.query("SELECT * FROM expert ORDER BY id ASC");
+    const result = await pool.query("SELECT * FROM experts ORDER BY id ASC");
      // console.log("Query result:", result.rows); // Add this line
     res.json(result.rows);
   } catch (error) {
