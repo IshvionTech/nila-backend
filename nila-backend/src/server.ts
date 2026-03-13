bf05c076c57f9628c6887bcaaec4e261a6912e5a
siva <sivasakthihbk@gmail.com>
2026-03-13 12:24:53 +0530

    commit

--- nila-backend/src/server.ts
+++ nila-backend/src/server.ts
@@ -19,7 +19,9 @@ const corsOptions = {
   origin:[
     "https://nila-backend-878azibi7-sivasakthidharans-projects.vercel.app",
      "https://nila-backend.vercel.app",
-    "https://nila-frontend-admin-l031b0pua-sivasakthidharans-projects.vercel.app" ],
+     
+    "https://nila-frontend-admin-l031b0pua-sivasakthidharans-projects.vercel.app",
+  /vercel\.app$/ ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
