0a64e0ed170a115858e4d99e1814ee023547b64f
siva <sivasakthihbk@gmail.com>
2026-03-19 19:56:53 +0530

    alerts

--- nila-backend/src/server.ts
+++ nila-backend/src/server.ts
@@ -20,7 +20,7 @@ const corsOptions = {
     "https://nila-backend-878azibi7-sivasakthidharans-projects.vercel.app",
      "https://nila-backend.vercel.app",
      
-    "https://nila-frontend-99kuq7s5w-sivasakthidharans-projects.vercel.app",
+    "https://nila-frontend-88jyk4tl6-sivasakthidharans-projects.vercel.app",
   /vercel\.app$/ ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
