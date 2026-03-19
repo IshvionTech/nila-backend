c33c70d3483a5c8302480a92fc58ba37a83642ee
siva <sivasakthihbk@gmail.com>
2026-03-19 14:42:23 +0530

    changed

--- nila-backend/src/server.ts
+++ nila-backend/src/server.ts
@@ -20,7 +20,7 @@ const corsOptions = {
     "https://nila-backend-878azibi7-sivasakthidharans-projects.vercel.app",
      "https://nila-backend.vercel.app",
      
-    "https://nila-admin-b6605j0dy-sivasakthidharans-projects.vercel.app",
+    "https://nila-frontend-96xjo5hex-sivasakthidharans-projects.vercel.app",
   /vercel\.app$/ ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
