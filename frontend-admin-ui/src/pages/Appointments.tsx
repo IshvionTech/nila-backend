2fa53f2ef04bd9d83c2c92e777fd2bedb9cf1aac
siva <sivasakthihbk@gmail.com>
2026-03-27 13:16:21 +0530

    email

--- frontend-admin-ui/src/pages/Appointments.tsx
+++ frontend-admin-ui/src/pages/Appointments.tsx
@@ -180,7 +180,9 @@ if(!/^\d{10}$/.test(formData.phone))  {
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({  email: formData.email })
+      body: JSON.stringify({  email: formData.email,
+         phone: formData.phone 
+        })
     });
 
      console.log("Response status:", res.status);
