a6cb24e764fb24d575a24247070f18b321e618a0
siva <sivasakthihbk@gmail.com>
2026-03-27 15:25:21 +0530

    gmail

--- nila-backend/src/controllers/appointment.controller.ts
+++ nila-backend/src/controllers/appointment.controller.ts
@@ -96,9 +96,10 @@ export const sendOtp = async (req: Request, res: Response) => {
   console.log("Generated OTP:", otp);
     // ✅ EMAIL TRANSPORT
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      //host: "smtp.gmail.com",
+      //port: 587,
+     // secure: false,
+     service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS   // app password
